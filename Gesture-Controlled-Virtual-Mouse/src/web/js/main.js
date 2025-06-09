b08b1c89a35354ea0256f551ace72d20dd9064cb
Subash.R <144207902+SubashR22@users.noreply.github.com>
2025-06-09 14:17:30 +0530

    Add files via upload

---
+++ Gesture-Controlled-Virtual-Mouse/src/web/js/main.js
@@ -0,0 +1,31 @@
+
+
+
+eel.expose(addUserMsg);
+eel.expose(addAppMsg);
+
+
+function addUserMsg(msg) {
+    element = document.getElementById("messages");
+    element.innerHTML += '<div class="message from ready rtol">' + msg + '</div>';
+    element.scrollTop = element.scrollHeight - element.clientHeight - 15;
+    //add delay for animation to complete and then modify class to => "message from"
+    index = element.childElementCount - 1;
+    setTimeout(changeClass.bind(null, element, index, "message from"), 500);
+}
+
+function addAppMsg(msg) {
+    element = document.getElementById("messages");
+    element.innerHTML += '<div class="message to ready ltor">' + msg + '</div>';
+    element.scrollTop = element.scrollHeight - element.clientHeight - 15;
+    //add delay for animation to complete and then modify class to => "message to"
+    index = element.childElementCount - 1;
+    setTimeout(changeClass.bind(null, element, index, "message to"), 500);
+}
+
+function changeClass(element, index, newClass) {
+    console.log(newClass +' '+ index);
+    element.children[index].className = newClass;
+}
+
+
